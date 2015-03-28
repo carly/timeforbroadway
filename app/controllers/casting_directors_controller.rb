b6f2afcecb63dde4d97bbad731aeed2d2cda1c3b
carobinson <carly.a.robinson@gmail.com>
2015-03-28 15:52:23 -0400

    updated actions for casting director controller + root route

--- app/controllers/casting_directors_controller.rb
+++ app/controllers/casting_directors_controller.rb
@@ -1,3 +1,13 @@
 class CastingDirectorsController < ApplicationController
+	def signup
+	end
+
+	def new
+		@castingdirector = CastingDirector.new
+	end
+
+	def create
+		@castingdirector = CastingDirector.new(castingdirector_params)
+	end
 
 end
