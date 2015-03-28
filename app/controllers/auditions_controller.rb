ed084d972a913ad35b6e65e049dbd967f707528f
Sandy-Cheeks <fun.n.sillyness@gmail.com>
2015-03-28 17:08:20 -0400

    comments to Audition Controller

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -1,13 +1,19 @@
 class AuditionController <ApplicationController
+# Refer to http://guides.rubyonrails.org/getting_started.html
+
 	def new
 	end
 
 	def create
+		# saves auditions
+		# redirects to auditions list view
 	end
 
 	def index
+		# show all auditions
 	end
 
 	def show
+		# show individual auditions
 	end
 end
