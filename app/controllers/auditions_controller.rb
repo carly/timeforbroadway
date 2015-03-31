8c84788e9ca05e89184d617dc9444c06ceeb3056
Sandy-Cheeks <fun.n.sillyness@gmail.com>
2015-03-31 18:28:42 -0400

    updated class name and rearranged CRUD elements

--- app/controllers/auditions_controller.rb
+++ app/controllers/auditions_controller.rb
@@ -1,6 +1,13 @@
-class AuditionController <ApplicationController
+class AuditionsController <ApplicationController
 # Refer to http://guides.rubyonrails.org/getting_started.html
+	def index
+		# show all auditions
+	end
 
+	def show
+		# show individual auditions
+	end
+	
 	def new
 	end
 
@@ -9,11 +16,58 @@ class AuditionController <ApplicationController
 		# redirects to auditions list view
 	end
 
+
+end
+
+=begin Example from Blog Tutorial:
+
+class ArticlesController < ApplicationController
 	def index
-		# show all auditions
-	end
+    	@articles = Article.all
+   	end
 
 	def show
-		# show individual auditions
+		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+	  @article = Article.find(params[:id])
+	end	
+
+	def create
+		@article = Article.new(article_params)
+ 
+  		if @article.save
+    		redirect_to @article
+  		else
+    		render 'new'
+  		end
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end	
+
+	def destroy
+	  @article = Article.find(params[:id])
+	  @article.destroy
+	 
+	  redirect_to articles_path
+	end
+
+	private
+  		def article_params
+    		params.require(:article).permit(:title, :text)
+  		end
 end
+=end
