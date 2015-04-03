b859490476679fcd221efe0a56b550b6a754f65d
Asia Lindsay <asialindsay@gmail.com>
2015-04-02 20:16:24 -0400

    commiting changes for first merge

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,3 +1,4 @@
 class ActorsController < ApplicationController
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328182431) do
+ActiveRecord::Schema.define(version: 20150328192239) do
+
+  create_table "actors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "auditions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "casting_directors", force: :cascade do |t|
     t.string   "name"
